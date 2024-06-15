43ed141da8d25d174ab2fb677d8fa1a5bf2eaed6
shyam chauhan <mitsme44@gmail.com>
2024-06-15 21:40:36 +0530

    services scan

--- port_scanner.py
+++ port_scanner.py
@@ -1,15 +1,14 @@
 import socket
 from tqdm import tqdm
 ports = []
-ip = input("Enter IP or domain you want to port scan :")
+ip = input("Enter IP or domain you want to port scan :" or "scanme.nmap.org")
 
-for port in (pbar :=tqdm(range(1, 65536))):  # Scan ports from 1 to 65535
+for port in (pbar :=tqdm(range(1,500))):  # Scan ports from 1 to 65535
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(1)  # Timeout for connection attempt (adjust as needed)
         try:
                 pbar.set_description("scanning port {}".format(port))
                 s.connect((ip, port))
-                print(f"Port {port} is open")
                 ports.append(port)
         except socket.error:
                 # Connection error means the port is closed or filtered
@@ -18,5 +17,7 @@ for port in (pbar :=tqdm(range(1, 65536))):  # Scan ports from 1 to 65535
                 s.close()
 
 print("Summary of open ports")
+print("service | port ")
 for port in ports:
-        print(port)
+        service = socket.getservbyport(port)
+        print("{}        {}".format(service,port))
